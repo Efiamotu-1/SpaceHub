a3170843d5e898cc0b4de86639531aae83f19b1b
Efiamotu-1 <musahabeebefy10@gmail.com>
2022-09-27 20:32:30 +0100

    add rendered list

--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -9,11 +9,11 @@ const Rockets = (props) => {
     id, name, description, image,
   } = singleRocket;
   return (
-    <div key={id} className="flex space-x-4 py-5  border-b border-green-800">
-      <div>
-        <img src={image} alt="rocket" className="rounded" />
+    <div key={id} className="md:flex space-x-4 py-5 border-b border-green-800">
+      <div className="md:w-1/4 ">
+        <img src={image} alt="rocket" className="object-contain rounded" />
       </div>
-      <div className="flex-col ">
+      <div className="flex-col md:w-3/4">
         <p className="text-gray-700">{name}</p>
         <h2 className="mb-5">{description}</h2>
         <button className="button bg-blue-500 hover:bg-blue-600 active:bg-blue-900 py-2 px-3 rounded text-stone-100" type="submit">Reserve Rockets</button>
