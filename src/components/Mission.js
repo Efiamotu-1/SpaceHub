415c6ede1c771f0ec7b67ddbc385198d85a153ae
Efiamotu-1 <musahabeebefy10@gmail.com>
2022-09-28 17:49:40 +0100

    fix bugs

--- src/components/Mission.js
+++ src/components/Mission.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
+import './mission.css';
 
 const Mission = (props) => {
   // eslint-disable-next-line no-unused-vars
