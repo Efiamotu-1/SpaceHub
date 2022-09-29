c274245da88dc03dc3da9ccd6ab9e37bf723560b
cmolinan <cmolinan@gmail.com>
2022-09-28 23:05:47 -0500

    fixing a linter error

--- src/__tests__/Navbar.test.js
+++ src/__tests__/Navbar.test.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
-import NavBar from '../components/NavBar';
+import Navbar from '../components/NavBar';
 
 describe('Navbar component', () => {
   test('Correct render of Navbar', () => {
     const navbar = render(
       <Router>
-        <NavBar />
+        <Navbar />
       </Router>,
     );
     expect(navbar).toMatchSnapshot();
@@ -17,7 +17,7 @@ describe('Navbar component', () => {
   test('Check if \'Space Traveler\'s Hub\' is in the rendered document of Navbar', () => {
     render(
       <Router>
-        <NavBar />
+        <Navbar />
       </Router>,
     );
     expect(screen.getByText('Space Traveler\'s Hub')).toBeInTheDocument();
